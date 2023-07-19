326bbf0541b74201e2661a6ea5e4f2a266393615
mys1024 <mys.1024@outlook.com>
2023-07-20 00:16:20 +0800

    test: add tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,23 @@
 import { describe, expect, it } from 'vitest'
-import { foo } from '../src/index'
+import { createEffect, createSignal } from '../src/index'
 
-describe('basic', () => {
-  it('foo', async () => {
-    expect(foo()).toBe('bar')
+describe('signal', () => {
+  it('set and get', async () => {
+    const [cnt, setCnt] = createSignal(0)
+    expect(cnt()).toBe(0)
+    setCnt(cnt() + 10)
+    expect(cnt()).toBe(10)
+  })
+})
+
+describe('effect', () => {
+  it('tracking', async () => {
+    const [cnt, setCnt] = createSignal(1)
+    let sum = 0
+    createEffect(() => {
+      sum += cnt()
+    })
+    setCnt(2)
+    expect(sum).toBe(3)
   })
 })
